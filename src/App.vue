a5ecba5c8b051ecb5024e17fa686212f8cef9fee
Rachelsowmya <118365554+Rachelsowmya@users.noreply.github.com>
2023-12-06 17:30:52 -0500

    vue

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <MyHeader />
+    <Listv :products="products" />
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import Listv from './components/Listv.vue' 
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    Listv
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    async fetchProducts() {
+      const res = await fetch('https://plant-nursery-85ik.onrender.com');
+      const data = await res.json()
+      console.log(data)
+      return data
+    }
+  },
+  async created() {
+  this.products = await this.fetchProducts(); 
+}
+
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+  max-width: 800px;
+  margin: auto;
+}
+</style>
---
+++ src/components/Listv.vue
@@ -0,0 +1,28 @@
+<template>
+   
+
+    <div v-for="pname of products" :key="pname.id">
+            <prodview :pname=pname />
+    </div>
+
+</template>
+
+<script>
+import prodview from './prodview.vue'
+
+export default{
+    name: 'Listv',
+    props:{
+        products: Array
+    },
+    components:{
+        prodview
+    }
+}
+
+</script>
+
+<style>
+
+
+</style>
